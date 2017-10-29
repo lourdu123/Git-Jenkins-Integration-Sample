e5176b54d0aec7b059c993a5f7f5b9af4cfb6e2c
DIGITALHARBOR\preddy <ponnapati.reddy@dharbor.com>
2017-10-29 21:32:50 +0530

    initial commit

---
+++ Git-Jenkins-Integration-example/src/main/java/com/example/GitJenkinsIntegrationexample/GitJenkinsIntegrationExampleApplication.java
@@ -0,0 +1,20 @@
+package com.example.GitJenkinsIntegrationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitJenkinsIntegrationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitJenkinsIntegrationExampleApplication.class, args);
+	}
+	
+	@RequestMapping("/test")
+	public String test(){
+		return "hollo world.";
+	}
+}
